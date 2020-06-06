c08d04719eef28a2b89d7cdecf1d44f0f9ae8863
Audris Mockus <audris@utk.edu>
2020-06-06 09:50:11 -0400

    fix sudo

--- startsvc.sh
+++ startsvc.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 i=$1
 sed -i 's/^$/+ : '$i' : ALL/' /etc/security/access.conf
-echo "icputrd:x:2344:audrislocal,adautart,audris,tsaul,dsteadma" >> /etc/group
+echo "$i ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/$i
 /usr/sbin/sssd -fd 2
 /usr/sbin/sshd -e
-cd /opt/mean.js
-export DB_1_PORT_27017_TCP_ADDR=db
-npm start &
-
-sudo -H -u $i sh -c /bin/notebook.sh 
-
 
 while true
 do echo here $(pwd) $(ps -ef | grep ssh) 
