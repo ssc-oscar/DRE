de039a11172ea1de86269e938bdbd3d4fe611c1f
Audris Mockus <audris@utk.edu>
2022-01-03 09:02:12 -0500

    improving web page

--- client/app/components/Home/HomeWOC.js
+++ client/app/components/Home/HomeWOC.js
@@ -14,29 +14,23 @@ class HomeWOC extends Component {
       <>
         <Row className="justify-content-center">
             <Card className="bg-secondary shadow border-0">
-              <CardHeader className="bg-transparent">
-                  <p>Listed below are some additional resources about the World of Code dataset</p>
-                  <ListGroup>
-                    <ListGroupItem>
-                      <a href="https://github.com/woc-hack/tutorial">Tutorial on the use of WoC</a>
-                    </ListGroupItem>
-                    <ListGroupItem>
-                      <a href="http://bit.ly/WoCSignup">Sign up to use WoC/participate in upcoming WoC hackathons</a>
-                    </ListGroupItem>
-                    <ListGroupItem>
-                      <a href="https://bitbucket.org/swsc/lookup/src/master/README.md">Details on WoC storage and the APIs</a>
-                    </ListGroupItem>
-                    <ListGroupItem>
-                      <a href="https://bitbucket.org/swsc/overview/src/master/README.md">Overview of the Software Supply Chains</a>
-                    </ListGroupItem>
-                    <ListGroupItem>
-                      <a href="https://bitbucket.org/swsc/overview/raw/master/pubs/WoC.pdf">Original paper</a>
-                    </ListGroupItem>
-                    <ListGroupItem>
-                      <a href="https://bitbucket.org/swsc/overview/src/master/fun/README.md">Fun Facts</a>
-                    </ListGroupItem>
-                  </ListGroup>
-              </CardHeader>
+            <CardHeader className="bg-transparent">
+            <p>How to use/cite World of Code:</p>
+            <ListGroup>
+            <ListGroupItem> <a href="https://github.com/woc-hack/tutorial">Tutorial</a></ListGroupItem>	    
+            <ListGroupItem> <a href="http://bit.ly/WoCSignup">Sign up to use WoC/participate in upcoming WoC hackathons</a></ListGroupItem>
+	    <ListGroupItem> <a href="https://bitbucket.org/swsc/overview/src/master/README.md">General WoC infrastructure overview</a> </ListGroupItem>
+            <ListGroupItem> <a href="https://bitbucket.org/swsc/lookup/src/master/README.md">More details on WoC APIs</a> </ListGroupItem>
+            <ListGroupItem> <a href="https://bitbucket.org/swsc/overview/src/master/fun/README.md">Fun facts about some crazy competitions among git repositories</a> </ListGroupItem>
+            <ListGroupItem>
+            <a href="https://bitbucket.org/swsc/overview/raw/master/pubs/WoC.pdf">Paper about WoC  (cite: @article{ma2021world,  title={World of code: Enabling a research workflow for mining and analyzing the universe of open source vcs data},
+                                              author={Ma, Yuxing and Dey, Tapajit and Bogart, Chris and Amreen, Sadika and Valiev, Marat and Tutko, Adam and Kennard, David and Zaretzki, Russell and Mockus, Audris},
+                                              journal={Empirical Software Engineering},
+													     volume={26},  number={2},  pages={1--42},  year={2021},  publisher={Springer}})</a>
+            </ListGroupItem>
+	    <ListGroupItem>   <a href="https://github.com/woc-hack/tutorial/blob/master/LICENSE">License</a>  </ListGroupItem>
+            </ListGroup>
+            </CardHeader>
             </Card>
         </Row>
       </>
