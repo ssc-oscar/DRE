e6d87d401418ab25b270dddbf317b4bb01d848cb
Audris Mockus <audris@utk.edu>
2022-01-03 09:11:27 -0500

    improving web page

--- client/app/components/Home/HomeWOC.js
+++ client/app/components/Home/HomeWOC.js
@@ -23,10 +23,7 @@ class HomeWOC extends Component {
             <ListGroupItem> <a href="https://bitbucket.org/swsc/lookup/src/master/README.md">More details on WoC APIs</a> </ListGroupItem>
             <ListGroupItem> <a href="https://bitbucket.org/swsc/overview/src/master/fun/README.md">Fun facts about some crazy competitions among git repositories</a> </ListGroupItem>
             <ListGroupItem>
-            <a href="https://bitbucket.org/swsc/overview/raw/master/pubs/WoC.pdf">Paper about WoC  (cite: @article{ma2021world,  title={World of code: Enabling a research workflow for mining and analyzing the universe of open source vcs data},
-                                              author={Ma, Yuxing and Dey, Tapajit and Bogart, Chris and Amreen, Sadika and Valiev, Marat and Tutko, Adam and Kennard, David and Zaretzki, Russell and Mockus, Audris},
-                                              journal={Empirical Software Engineering},
-													     volume={26},  number={2},  pages={1--42},  year={2021},  publisher={Springer}})</a>
+            <a href="https://bitbucket.org/swsc/overview/raw/master/pubs/WoC.pdf">Paper about WoC. Cite: \@article\{ma2021world,  title=\{World of code: Enabling a research workflow for mining and analyzing the universe of open source vcs data\}, author=\{Ma, Yuxing and Dey, Tapajit and Bogart, Chris and Amreen, Sadika and Valiev, Marat and Tutko, Adam and Kennard, David and Zaretzki, Russell and Mockus, Audris\}, journal=\{Empirical Software Engineering\}, volume=26,  number=2, pages={1--42},  year=2021, publisher=Springer\}</a>
             </ListGroupItem>
 	    <ListGroupItem>   <a href="https://github.com/woc-hack/tutorial/blob/master/LICENSE">License</a>  </ListGroupItem>
             </ListGroup>
